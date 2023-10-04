53a3d1685241d1c467457486e2baf6abf7c37f87
Amir Raminfar <findamir@gmail.com>
2023-10-04 14:19:50 -0700

    uses lighnting css

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import svgLoader from "vite-svg-loader";
 import UnoCSS from "unocss/vite";
 
 export default defineConfig({
+  css: {
+    transformer: "lightningcss",
+  },
   plugins: [UnoCSS(), vuePlugin(), svgLoader()],
 });
